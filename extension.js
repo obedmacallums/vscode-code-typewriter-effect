227dd11119015de30a351a84f528ec0a8f20b7bb
Obed Macallums <obedmacallums@gmail.com>
2024-05-27 20:56:43 -0400

    added eol

--- extension.js
+++ extension.js
@@ -3,102 +3,110 @@ const vscode = require("vscode");
 /**
  * @param {vscode.ExtensionContext} context
  */
-
 function activate(context) {
-	console.log(
-		'Congratulations, your extension "code-typewriter-effect" is now active!'
-	);
+    console.log(
+        'Congratulations, your extension "code-typewriter-effect" is now active!'
+    );
 
-	let typingProcessRunning = false; // Variable to track if the typing process is running
+    let typingProcessRunning = false; // Variable to track if the typing process is running
 
-	let typingSpeed = vscode.workspace
-		.getConfiguration("code-typewriter-effect")
-		.get("codeTypewriterEffectTypingSpeed");
+    let typingSpeed = vscode.workspace
+        .getConfiguration("code-typewriter-effect")
+        .get("codeTypewriterEffectTypingSpeed");
 
-	console.log(typingSpeed);
+    console.log(typingSpeed);
 
-	let disposableType = vscode.commands.registerCommand(
-		"code-typewriter-effect.retype",
-		function () {
-			if (typingProcessRunning) {
-				return; // If the typing process is already running, do nothing
-			}
+    let disposableType = vscode.commands.registerCommand(
+        "code-typewriter-effect.retype",
+        function () {
+            if (typingProcessRunning) {
+                return; // If the typing process is already running, do nothing
+            }
 
-			typingProcessRunning = true; // Mark that the typing process has started
+            typingProcessRunning = true; // Mark that the typing process has started
 
-			let editor = vscode.window.activeTextEditor;
-			if (!editor) {
-				return;
-			}
+            let editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                return;
+            }
 
-			let currentPosition = 0;
-			let text = editor.document.getText();
+            let text = editor.document.getText();
+            let eol = editor.document.eol === vscode.EndOfLine.CRLF ? "\r\n" : "\n"; // Detect EOL
 
-			editor
-				.edit((editBuilder) => {
-					editBuilder.delete(
-						new vscode.Range(0, 0, editor.document.lineCount, 0)
-					);
-				})
-				.then(() => {
-					type();
-				});
+            editor
+                .edit((editBuilder) => {
+                    editBuilder.delete(
+                        new vscode.Range(0, 0, editor.document.lineCount, 0)
+                    );
+                })
+                .then(() => {
+                    type(0, 0);
+                });
 
-			function type() {
-				if (!typingProcessRunning) {
-					return; // If the typing process is stopped, exit the function
-				}
+            function type(lineIndex, charIndex) {
+                if (!typingProcessRunning) {
+                    return; // If the typing process is stopped, exit the function
+                }
 
-				let line = text.split("\n")[currentPosition];
-				let i = 0;
+                let lines = text.split(eol);
+                if (lineIndex >= lines.length) {
+                    typingProcessRunning = false; // Typing process is complete, reset the variable
+                    return;
+                }
 
-				function insertNextCharacter() {
-					if (!typingProcessRunning) {
-						return; // If the typing process is stopped, exit the function
-					}
+                let line = lines[lineIndex];
 
-					if (i <= line.length) {
-						editor
-							.edit((editBuilder) => {
-								editBuilder.insert(
-									new vscode.Position(currentPosition, i),
-									line.charAt(i)
-								);
-							})
-							.then(() => {
-								i++;
-								setTimeout(insertNextCharacter, typingSpeed);
-							});
-					} else {
-						currentPosition++;
-						if (currentPosition < text.split("\n").length) {
-							line = text.split("\n")[currentPosition];
-							i = 0;
-							setTimeout(insertNextCharacter, typingSpeed);
-						} else {
-							typingProcessRunning = false; // Typing process is complete, reset the variable
-						}
-					}
-				}
+                function insertNextCharacter() {
+                    if (!typingProcessRunning) {
+                        return; // If the typing process is stopped, exit the function
+                    }
 
-				insertNextCharacter();
-			}
-		}
-	);
+                    if (charIndex <= line.length) {
+                        editor
+                            .edit((editBuilder) => {
+                                editBuilder.insert(
+                                    new vscode.Position(lineIndex, charIndex),
+                                    line.charAt(charIndex) || ""
+                                );
+                            })
+                            .then(() => {
+                                charIndex++;
+                                setTimeout(insertNextCharacter, typingSpeed);
+                            });
+                    } else {
+                        // Move to the next line without adding an extra newline when it's the last line
+                        lineIndex++;
+                        charIndex = 0;
+                        if (lineIndex < lines.length) {
+                            editor
+                                .edit((editBuilder) => {
+                                    editBuilder.insert(new vscode.Position(lineIndex - 1, lines[lineIndex - 1].length), eol);
+                                })
+                                .then(() => {
+                                    setTimeout(() => type(lineIndex, charIndex), typingSpeed);
+                                });
+                        }
+                    }
+                }
 
-	let disposableStop = vscode.commands.registerCommand(
-		"code-typewriter-effect.stop",
-		function () {
-			typingProcessRunning = false; // When the "code-typewriter-effect.stop" command is executed, stop the typing process
-		}
-	);
+                insertNextCharacter();
+            }
+        }
+    );
 
-	context.subscriptions.push(disposableType, disposableStop);
+    let disposableStop = vscode.commands.registerCommand(
+        "code-typewriter-effect.stop",
+        function () {
+            typingProcessRunning = false; // When the "code-typewriter-effect.stop" command is executed, stop the typing process
+        }
+    );
+
+    context.subscriptions.push(disposableType, disposableStop);
 }
 
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate,
+    activate,
+    deactivate,
 };
